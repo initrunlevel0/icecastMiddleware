74054457574f70ec99712b15976e47ad6bd455f8
Putu Wiramaswara Widya <wira@ta.wirama.web.id>
2014-12-26 10:16:50 +0700

    uji coba

--- uji/index.js
+++ uji/index.js
@@ -2,74 +2,55 @@
 
 var spawn = require('child_process').spawn;
 var async = require('async');
+var moment = require('moment');
 var fs = require('fs');
+
 require('events').EventEmitter.prototype._maxListeners = 1000000;
 
 
 // Configuration
-var servers = [
-    "10.151.36.205:8001",
-    "10.151.36.205:8002",
-    "10.151.36.205:8003"
-];
+var server = process.argv[2];
+var mountPoint = process.argv[3];
+var instancesPerServer = process.argv[4];
+var timePerRequest = process.argv[5];
 
-var testNodes = [
-    {ip: "10.151.36.27", user: "praktikum"},
-    {ip: "10.151.36.34", user: "praktikum"},
-    {ip: "10.151.36.39", user: "wira"}
-];
-
-var mountPoint = "/halo"
-
-var timePerRequest = 300;
-var instancesPerServer = 100;
+var success = 0;
+var fail = 0;
 
 // Create array with instancePerServer times element for the sake of async library
 var arr = []
 for(var i=0; i < instancesPerServer; i++) {
     arr.push(i);
 }
+async.each(arr, function(i, callback) {
+    // Do a curl
+    var stdout = "";
+    var start = moment(new Date());
+    var ps = spawn('curl', ['-o',  '/dev/null', '-m', timePerRequest, '-s',
+                   '-w', "%{size_download}", 'http://' + server + mountPoint]);
 
-async.series([
-function(callback) {
-    // Copy script file
-    async.each(testNodes, function(testNode, callback) {
-        var ps = spawn("scp", ["./uji.sh", testNode.user + "@" + testNode.ip + ":~/uji.sh"])
-        ps.stderr.pipe(process.stdout);
-        ps.on('exit', function() {
-            callback();
-        });
-    }, function(err) {
+
+    ps.stdout.on('data', function(data) {
+        stdout += data.toString();
+    });
+
+    ps.stderr.pipe(process.stdout);
+
+    ps.on('exit', function() {
+        var end = moment(new Date());
+        var result = parseFloat(stdout) / 1024;
+        var diff =  moment.duration(end.diff(start));
+
+        if(diff.asSeconds() < timePerRequest) {
+            fail++;
+        } else {
+            success++;
+        }
+
         callback();
     });
 
-}, function(callback) {
-    // Execute it
-    async.eachSeries(servers, function(server, callback) {
-        // Open file for the result
-        var output = fs.createWriteStream(server, {flags: 'w'});
 
-        async.each(testNodes, function(testNode, callback) {
-            var stdout = "";
-            var ps = spawn("ssh", ['-vv', testNode.user + "@" + testNode.ip, "~/uji.sh",
-                           instancesPerServer, timePerRequest, "http://" + server + mountPoint]);
-
-            ps.stderr.pipe(process.stdout);
-
-            ps.stdout.on('data', function(data) {
-                output.write(data);
-            });
-
-            ps.on('close', function() {
-                callback();
-            });
-        }, function(err) {
-            output.end();
-            callback();
-        });
-    }, function(err) {
-        callback();
-    });
-}
-], function(err) {
+}, function(err) {
+    console.log(success)
 });
